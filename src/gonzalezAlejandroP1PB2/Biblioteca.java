080b9d96d2a299be3c6057141e54d2a6d98f1724
Alejandro González <aledagonale@gmail.com>
2021-06-01 20:36:28 -0300

    Agregados metodos para libros en biblioteca

--- src/gonzalezAlejandroP1PB2/Biblioteca.java
+++ src/gonzalezAlejandroP1PB2/Biblioteca.java
@@ -5,8 +5,38 @@ import java.util.ArrayList;
 
 public class Biblioteca {
 
+    
     private ArrayList<Libro> libros = new ArrayList<Libro>();
     private ArrayList<Prestamo> prestamos = new ArrayList<Prestamo>();
+    private String nombre;
+    
+    public Biblioteca(String nombre) {
+	this.nombre = nombre;
+    }
+
+    public ArrayList<Libro> getLibros() {
+        return libros;
+    }
+
+    public ArrayList<Prestamo> getPrestamos() {
+        return prestamos;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void agregarLibro(Libro libro) {
+	
+	libros.add(libro);
+	
+    }
+
+    public int cantidadDeLibros() {
+	return libros.size();
+    }
+    
+   
     
     
 }
